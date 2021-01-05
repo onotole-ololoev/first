ed25c65c190ccf31dbf37b8b129775b5893bca13
onotole-ololoev <tolya.tar@gmail.com>
2021-01-05 21:01:49 +0300

    console programm films

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("hello!");
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+
+let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+    lastFilmRate = prompt('На сколько оцение его?', ''),
+    lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
+    lastFilmRate2 = prompt('На сколько оцение его?', '');
+
+    personalMovieDB.movies[lastFilm] = lastFilmRate;
+    personalMovieDB.movies[lastFilm2] = lastFilmRate2;  
+
+    console.log(personalMovieDB);
+    
