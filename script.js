426420b586ba512986540ed4d6ea53d2f8b1fdb1
onotole-ololoev <tolya.tar@gmail.com>
2021-01-07 10:44:44 +0300

    while for if practice

--- script.js
+++ script.js
@@ -8,13 +8,27 @@ genres: [],
 privat: false
 };
 
-let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    lastFilmRate = prompt('На сколько оцение его?', ''),
-    lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
-    lastFilmRate2 = prompt('На сколько оцение его?', '');
 
-    personalMovieDB.movies[lastFilm] = lastFilmRate;
-    personalMovieDB.movies[lastFilm2] = lastFilmRate2;  
+    for (let i = 0; i < 2;  i ++) {
+        let a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцение его?', '');
+        if (a.length === 0 || a.length > 50 || a === null || b.length === 0 || b.length > 50 || b === null) {
+            a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцение его?', '');
+        }   
+        personalMovieDB.movies[a] = b;
+    }
+
+    if (personalMovieDB.count < 10 ) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
+
 
     console.log(personalMovieDB);
     
